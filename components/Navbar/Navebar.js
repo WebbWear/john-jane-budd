ea3f79b7247f14d976496e641c8f6d921380c94e
Jeff Webb <webbwear@icloud.com>
2021-04-28 20:47:25 -0400

    Working on tabs navbar

--- components/Navbar/Navebar.js
+++ components/Navbar/Navebar.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { AppBar, Tabs, Tab, Toolbar, Typography } from '@material-ui/core'
 
 const Navebar = () => {
     return (
-        <div>
-           Navbar 
-        </div>
+        <>
+            <AppBar>
+                <Toolbar>
+                    <Typography>Header</Typography>
+
+                    <Tabs>
+                        <Tab label='Home'/>
+                    </Tabs>
+                    <Tabs>
+                        <Tab label='About'/>
+                    </Tabs>
+                    <Tabs>
+                        <Tab label='Contact'/>
+                    </Tabs>
+                </Toolbar>
+            </AppBar> 
+        </>
     )
 }
 
